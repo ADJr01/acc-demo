3f1f3238077f4a11952506b23c1ff12cdebba02e
Rahat Adnan <sthep90@gmail.com>
2025-11-22 22:45:40 +0600

    daisy ui and tailwind css integration in ember.js

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'acc-demo/config/environment';
 import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
+import 'acc-demo/app.css';
 
 if (macroCondition(isDevelopingApp())) {
   importSync('./deprecation-workflow');

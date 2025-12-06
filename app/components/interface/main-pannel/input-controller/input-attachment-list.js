01ee7aa5b010473969aba41f4f989ae292e81338
Rahat Adnan <sthep90@gmail.com>
2025-12-06 23:46:46 +0600

    file-attachment delete on progress

--- app/components/interface/main-pannel/input-controller/input-attachment-list.js
+++ app/components/interface/main-pannel/input-controller/input-attachment-list.js
@@ -1,8 +1,12 @@
 import Component from '@glimmer/component';
-import {action} from '@ember/object';
+import {action,set} from '@ember/object';
 
 export default class InterfaceMainPannelInputControllerInputAttachmentList extends Component {
-
+    @action deleteAttachment(attachmentIndex){
+      let attachment= this.args.attachmentList;
+      attachment = this.args.attachmentList.filter((item,item_index)=>item_index !== attachmentIndex)
+      set(this.args,'attachmentList',attachment)
+    }
 
 
 }
